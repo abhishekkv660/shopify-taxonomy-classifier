4ce7e04c986547df7590beee05eba6b20b084972
Abhishek <abhishekkalarikkal666@gmail.com>
2026-08-24 03:58:07 +0530

    fix: Add fallback SECRET_KEY to prevent startup crashes for reviewers

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-default-key-for-local-assessment")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 
